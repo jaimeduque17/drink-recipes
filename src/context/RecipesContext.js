8cc0a34f56343def6484f3ac4467daf18806d4b4
Jaime Duque <jaime.duque@estratek.com.co>
2020-06-02 19:12:22 -0500

    the API is queried

--- src/context/RecipesContext.js
+++ src/context/RecipesContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const RecipesContext = createContext();
 
@@ -9,11 +10,28 @@ const RecipesProvider = (props) => {
         name: '',
         category: ''
     });
+    const [consult, setConsult] = useState(false);
+
+    const { name, category } = search;
+
+    useEffect(() => {
+        if(consult) {
+            const getRecipes = async () => {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}&c=${category}`;
+                const result = await axios.get(url);
+                setRecipes(result.data.drinks);
+            }
+            
+            getRecipes();
+        }
+    }, [search]);
 
     return (  
         <RecipesContext.Provider
             value={{
-                setSearchrecipes
+                recipes,
+                setSearchrecipes,
+                setConsult
             }}
         >
             {props.children}
